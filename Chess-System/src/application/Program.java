0bd082ab78fe53043b6b90022791db7abc49ed8f
Pedro Henrique <ph8584774@gmail.com>
2024-03-18 21:17:09 -0300

    First class position

---
+++ Chess-System/src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 6);
+        System.out.println(pos);
+    }
+}
