5aa6449008675a63f55bd73a32661707451ca6f2
Pedro Henrique <pedro.henrique@itpower.co>
2024-03-26 15:51:00 -0300

    Piece move count

--- Chess-System/src/application/UI.java
+++ Chess-System/src/application/UI.java
@@ -65,7 +65,7 @@ public class UI {
             }
         } else {
             System.out.println("CHECKMATE!");
-            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+            System.out.println("Congratulations " + chessMatch.getCurrentPlayer() + " you won!");
         }
     }
 
